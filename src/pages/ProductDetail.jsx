6ac4441d8487f6a9aee07d849d7bc629fc70a70d
dongyu-youn <>
2024-03-25 14:04:33 +0900

    리팩토링

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -4,19 +4,22 @@ import "slick-carousel/slick/slick-theme.css";
 import ProductCard from "../components/ProductCard";
 import { IoChevronForwardOutline, IoChevronBackOutline } from "react-icons/io5";
 
+// NextArrow 컴포넌트
 const NextArrow = (props) => (
   <div {...props}>
     <IoChevronForwardOutline size={56} className="align-middle" />
   </div>
 );
 
+// PrevArrow 컴포넌트
 const PrevArrow = (props) => (
   <div {...props}>
     <IoChevronBackOutline size={56} className="align-middle" />
   </div>
 );
 
-export default function ProductDetail({ title }) {
+// 슬라이더 컴포넌트
+const ImageSlider = ({ images }) => {
   const sliderRef = useRef(null); // 슬라이더 ref 생성
 
   const handleScroll = (e) => {
@@ -27,39 +30,6 @@ export default function ProductDetail({ title }) {
       sliderRef.current.slickPrev(); // 마우스 휠을 위로 스크롤할 때 이전 슬라이드로 전환
     }
   };
-
-  // 이미지 배열
-  const images = [
-    {
-      imageUrl:
-        "https://firebasestorage.googleapis.com/v0/b/wpoint-1d1be.appspot.com/o/202401181850138406525.jpg?alt=media&token=4be48c4c-ca09-43d3-ab65-0f2b96b5e74d",
-      title: "도배 하자 질의 응답 처리 : 한솔데코 시즌2 AI 경진대회",
-      content: "Product Description",
-      rank: "1",
-    },
-    {
-      imageUrl:
-        "https://firebasestorage.googleapis.com/v0/b/wpoint-1d1be.appspot.com/o/202309260935017642082.jpg?alt=media&token=639bdce4-9790-44b9-82bc-12057c938b92",
-      title: "2023 제2회 국방 AI 경진대회 MAICON",
-      content: "Product Description",
-      rank: "2",
-    },
-    {
-      imageUrl:
-        "https://firebasestorage.googleapis.com/v0/b/wpoint-1d1be.appspot.com/o/SW%E1%84%8C%E1%85%AE%E1%86%BC%E1%84%89%E1%85%B5%E1%86%B7%E1%84%83%E1%85%A2%E1%84%92%E1%85%A1%E1%86%A8%E1%84%89%E1%85%A1%E1%84%8B%E1%85%A5%E1%86%B8%E1%84%83%E1%85%A1%E1%86%AB%20_%20%E1%84%83%E1%85%A2%E1%84%92%E1%85%A1%E1%86%A8%E1%84%89%E1%85%A2%E1%86%BC%20%E1%84%92%E1%85%A2%E1%84%8F%E1%85%A5%E1%84%90%E1%85%A9%E1%86%AB%20%E1%84%80%E1%85%A7%E1%86%BC%E1%84%8C%E1%85%B5%E1%86%AB%E1%84%83%E1%85%A2%E1%84%92%E1%85%AC(%E1%84%85%E1%85%A9%E1%84%87%E1%85%A9%E1%86%BA%E1%84%87%E1%85%A2%E1%84%90%E1%85%B3%E1%86%AF)%20_%20%E1%84%8E%E1%85%A1%E1%86%B7%E1%84%80%E1%85%A1%E1%84%90%E1%85%B5%E1%86%B7%20%E1%84%86%E1%85%A9%E1%84%8C%E1%85%B5%E1%86%B8%E1%84%80%E1%85%A9%E1%86%BC%E1%84%80%E1%85%A9%20%20.jpeg?alt=media&token=fcbe6a14-3183-43a9-a11c-f5fc560dd201",
-      title: "2023 K-EDU 창의력 경진대회",
-      content: "Product Description",
-      rank: "3",
-    },
-    {
-      imageUrl:
-        "https://firebasestorage.googleapis.com/v0/b/wpoint-1d1be.appspot.com/o/SW%E1%84%8C%E1%85%AE%E1%86%BC%E1%84%89%E1%85%B5%E1%86%B7%E1%84%83%E1%85%A2%E1%84%92%E1%85%A1%E1%86%A8%E1%84%89%E1%85%A1%E1%84%8B%E1%85%A5%E1%86%B8%E1%84%83%E1%85%A1%E1%86%AB%20_%20%E1%84%83%E1%85%A2%E1%84%92%E1%85%A1%E1%86%A8%E1%84%89%E1%85%A2%E1%86%BC%20%E1%84%92%E1%85%A2%E1%84%8F%E1%85%A5%E1%84%90%E1%85%A9%E1%86%AB%20%E1%84%80%E1%85%A7%E1%86%BC%E1%84%8C%E1%85%B5%E1%86%AB%E1%84%83%E1%85%A2%E1%84%92%E1%85%AC(%E1%84%85%E1%85%A9%E1%84%87%E1%85%A9%E1%86%BA%E1%84%87%E1%85%A2%E1%84%90%E1%85%B3%E1%86%AF)%20_%20%E1%84%8E%E1%85%A1%E1%86%B7%E1%84%80%E1%85%A1%E1%84%90%E1%85%B5%E1%86%B7%20%E1%84%86%E1%85%A9%E1%84%8C%E1%85%B5%E1%86%B8%E1%84%80%E1%85%A9%E1%86%BC%E1%84%80%E1%85%A9%20%20.jpeg?alt=media&token=fcbe6a14-3183-43a9-a11c-f5fc560dd201",
-      title: "2023 K-EDU 창의력 경진대회",
-      content: "Product Description",
-      rank: "4",
-    },
-    // 추가적인 이미지들을 넣어주세요
-  ];
 
   // 슬라이더 설정
   const settings = {
@@ -93,8 +63,6 @@ export default function ProductDetail({ title }) {
         className="h-screen border-t border-b border-white relative"
         style={{ width: "100vw" }}
       >
-        {" "}
-        {/* 슬라이더 컨테이너의 너비 설정 */}
         {/* 제목 */}
         <p className="text-white bg-opacity-50 text-8xl custom-font top-4 left-8 p-4 ml-4 ">
           대 회 추 천
@@ -120,4 +88,6 @@ export default function ProductDetail({ title }) {
       </div>
     </div>
   );
-}
+};
+
+export default ImageSlider;
