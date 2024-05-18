5760591dbdba0655e533fda7db8dafba4ab34a50
dongyu-youn <donghe1472@naver.com>
2024-05-18 16:00:20 +0900

    [Feat] 대회신청

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Button({ text, onClick }) {
   return (
     <button
-      className="bg-gray-500 font-diphylleia font-bold text-2xl text-white py-2 px-4 rounded-sm hover:brightness-110 mt-16 hover:bg-black"
+      className="bg-gray-500 font-diphylleia font-bold text-2xl text-white py-2 px-4 rounded-sm hover:brightness-110 mt-4 hover:bg-black"
       onClick={onClick}
     >
       {text}
--- src/pages/PictureDetail.jsx
+++ src/pages/PictureDetail.jsx
@@ -4,12 +4,17 @@ import { useParams } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import Button from "../components/Button";
 import Footer from "../components/Footer";
+import ModalComponent from "../components/Modal";
+import Cookies from "js-cookie";
 
 function PictureDetail() {
+  const [video, setVideo] = useState(null);
+  const [apply, setApply] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 열림/닫힘 상태를 저장하는 state
+
   const location = useLocation();
   const pathname = location.pathname;
   const id = pathname.substring(pathname.lastIndexOf("/") + 1);
-  const [video, setVideo] = useState(null);
 
   useEffect(() => {
     const fetchVideo = async () => {
@@ -25,13 +30,103 @@ function PictureDetail() {
 
     fetchVideo();
   }, [id]);
+
+  useEffect(() => {
+    if (video) {
+      checkApplyStatus();
+    }
+  }, [video]);
+
+  const checkApplyStatus = async () => {
+    const userToken = Cookies.get("csrftoken") || "";
+
+    const axiosInstance = axios.create({
+      withCredentials: true,
+      headers: {
+        "X-CSRFToken": userToken,
+      },
+    });
+    try {
+      // 좋아요 상태를 확인하는 요청을 보냄
+      const response = await axiosInstance(
+        "http://127.0.0.1:8000/users/me/apply/"
+      );
+      // 동영상 ID가 있는지 확인
+      const isApply = response.data.some((fav) => fav.id === video.id);
+
+      // 좋아요 상태를 업데이트
+      setApply(isApply);
+    } catch (error) {
+      console.error("Error checking liked status:", error);
+    }
+  };
+
+  const toggleLike = async (e) => {
+    e.stopPropagation(); // 클릭 이벤트가 부모로 전파되지 않도록 함
+    e.preventDefault();
+
+    try {
+      // 클라이언트에서 좋아요 상태를 토글하면서 백엔드로 전송할 대회의 ID
+      const contestId = video.id;
+
+      // 좋아요 상태를 토글합니다.
+      const newLiked = !apply;
+      setApply(newLiked); // 새로운 좋아요 상태를 설정
+
+      const userToken = Cookies.get("csrftoken") || "";
+
+      const axiosInstance = axios.create({
+        withCredentials: true,
+        headers: {
+          "X-CSRFToken": userToken,
+        },
+      });
+
+      // 서버로 좋아요 상태를 전송합니다.
+      await axiosInstance.put("http://127.0.0.1:8000/users/me/apply/", {
+        id: contestId,
+      });
+
+      // 좋아요 상태를 확인하여 다시 설정합니다.
+      checkApplyStatus();
+    } catch (error) {
+      console.error("Error toggling like:", error);
+      // 에러 처리 로직 추가
+    }
+  };
+
+  // 모달 열기 함수
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  // 모달 닫기 함수
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  // 버튼 클릭 시 모달 열기
+  const handleButtonClick = () => {
+    openModal();
+  };
+
+  // 모달 영역 외의 곳을 클릭했을 때 모달을 닫기 위한 함수
+  const handleOutsideClick = (e) => {
+    if (e.target.classList.contains("modal-overlay")) {
+      closeModal();
+    }
+  };
+
   if (!video) {
     return <div>Loading...</div>;
   }
-  // 받은 상태 정보 사용하기
+
   return (
     <>
-      <section className="flex flex-col md:flex-row p-4 items-center justify-center">
+      <section
+        className="flex flex-col md:flex-row p-4 items-center justify-center"
+        onClick={handleOutsideClick}
+      >
         <img src={video.사진} className="w-1/6 px-20 basis-7/12" />
 
         <div className="w-full basis-5/12 flex flex-col p-4">
@@ -58,9 +153,22 @@ function PictureDetail() {
             참고링크: {video.참고링크}
           </p>
 
-          <Button className="mt-20" text="제의하기"></Button>
+          <Button
+            className="mt-20"
+            text="팀원조회"
+            onClick={handleButtonClick}
+          ></Button>
+
+          {apply ? (
+            <Button className="" text="완료" onClick={toggleLike}></Button>
+          ) : (
+            <Button className="" text="신청하기" onClick={toggleLike}></Button>
+          )}
         </div>
       </section>
+
+      {/* 모달 */}
+      {isModalOpen && <ModalComponent closeModal={closeModal} />}
 
       <Footer />
     </>
